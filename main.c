cb6cdc833d877a3d3a0ea1c923e5071f1a11eb78
Phelps Williams <phelpsw@gmail.com>
2016-08-06 14:19:30 -0700

    Cleaning up comments to match implementation, this works as an unfiltered frequency multiplier.

--- main.c
+++ main.c
@@ -3,55 +3,24 @@
 #include <util/delay.h>
 
 /**
- * The tach and speedo fire an interrupt on their rising edge.  The interrupt
- * handler stores the number of timer counts that have transpired in the period
- * between interrupts.  The period of a timer count is known so the effective
- * interrupt frequency can be calculated.  The function for converting a
- * tach / speedo sensor frequency to a speed / rpm is known which lets us
- * figure out how to properly display this information on the display panel.
+ * Using the Timer0 8 bit counter, the input pin clocks the counter.  At the
+ * control rate (10Hz) Timer0 is read.  Timer2 is setup to toggle TOCC0 and
+ * TOCC1 pins when Timer2 matches the Output Compare Register.  When the
+ * Timer0 value is read at the control rate, the corresponding OCR register
+ * value is calculated based on the Timer2 clock rate and the desired
+ * multiplier.  This sets the outputs to generate the desired output frequency.
  *
- * AVR203: http://www.atmel.com/Images/doc8365.pdf
+ * Board Labels:
+ * PB2 - Tach in
+ * PA7 - Speedo in
+ * PA1 - Speedo out
+ * PA2 - Tach out
+ * PA3 - Cruise control out
  *
- * Reference Oscillator 8MHz
- *
- * PB2 - Tach in - PCINT10 or ICP2
- * PA7 - Speedo in - PCINT7 or ICP1
- * PA1 - Speedo out - TOCC0
- * PA2 - Tach out - TOCC1
- * PA3 - Cruise control out - TOCC2
- *
- *
- * Timer2 - 8MHz / 1024 -> 7812.5Hz
- * 7812.5 / 65535 -> 0.119Hz Resolution
- *
- * I swapped TOCC2 and TOCC3 because it maps to output compare registers in an
- * advantageous way.
- *
- * OC2B -> TOCC0, TOCC2 - Speedo Frequency Output
- * OC2A -> TOCC3 - Tach Frequency Output
- *
- * TOCPMSA1 = 0x00;
- * TOCPMSA0 = 0;
- * TOCPMSA0 = (1 << TOCC0S1) | (1 << TOCC2S1) | (1 << TOCC3S1);
- * TOCPMCOE = (1 << TOCC0OE) | (1 << TOCC2OE) | (1 << TOCC3OE);
- *
- * OCR2AH / OCR2AL - Output compare register 2A - Tach Freq
- * OCR2BH / OCR2BL - Output compare register 2B - Speedo Freq
- *
- *
- *
- * TODO: Map ICP1 -> ICR1 - Speedo
- * TODO: Map ICP2 -> ICR2 - Tach
- *
- * ICR1H/ICR1L - Input Capture Register 1 - Speedo
- * ICR1H/ICR2L - Input Capture Register 2 - Tach
- *
- *
- * TODO: In normal timer mode, do i need to update the OCR registers with the
- * new delta every time it is met?  If I run Clear Timer on Compare Match it
- * helps this situation but makes the input capture side of things complicated
- *
- * Using output compare in normal mode is not recomended!!
+ * Repurposed Pin Names
+ * PA3 - Input (Label: Cruise)
+ * PA1 - Output1 (Label: Speedo)
+ * PA2 - Output2 (Label: Tach)
  *
  * Low frequency reproduction is important, Speedometer 18Hz input is ~6mph
  * Input Frequencies
@@ -71,25 +40,6 @@
  * 10 * (6.28 / 18.86) = 3.3mph
  * 10 * (10 / 18.86) = 5.3kph
  *
- * Plan:
- *
- * Use Timer0 (8bit) output compares for two output channels (Normal)
- * Use Timer1/2 (16bit) input compare for two input channels (CTC)
- *
- * Use output compareA/B on timer0 for setting frequency
- *  - handle interrupt to update next target counter value
- *  - Pick lowest frequency interrupt source to handle reading timer1/2 and
- *  calculating associated timer0 OCR values
- *
- */
-
-
-/*
- * Step 1 At 10Hz Read counter value
- * Step 2 Add counter value to 10 slot circular buffer which pops one value off
- *        the end, subtracts it from a total, then adds the new value on.
- * Step 3 If counter value is > ~10, use the counter directly, otherwise use
- *        rolling average to approximate speed
  */
 
 int main(void)
@@ -97,13 +47,9 @@ int main(void)
     // Set PA1 and PA2 as outputs
     DDRA |= _BV(DDA1); // TOCC0
     DDRA |= _BV(DDA2); // TOCC1
-    //DDRA |= _BV(DDA3); // TOCC2
 
     // Set PA7, PB2, PA4, PA3 as inputs
-    //DDRA &= ~_BV(DDA7);
-    //DDRB &= ~_BV(DDB2);
     DDRA &= ~_BV(DDA3); // T0
-    DDRA &= ~_BV(DDA4); // T1
 
     /*
      * Counter1 Configuration
